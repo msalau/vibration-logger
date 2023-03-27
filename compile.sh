03517a89e28c31d6c0a59184f2a367d12a794a7f
Maksim Salau <maksim.salau@gmail.com>
2023-03-27 20:45:06 +0200

    Rework CI

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+set -x
+
+COMPILE_ARGS="
+    --export-binaries
+    --build-property compiler.c.extra_flags=-DUSE_SPI_ARRAY_TRANSFER=1
+    --build-property compiler.cpp.extra_flags=-DUSE_SPI_ARRAY_TRANSFER=1
+"
+
+arduino-cli compile $COMPILE_ARGS --profile xiao ./XiaoLogger
+arduino-cli compile $COMPILE_ARGS --profile nano ./NanoLogger
+#arduino-cli compile $COMPILE_ARGS --profile pico ./PicoLogger
